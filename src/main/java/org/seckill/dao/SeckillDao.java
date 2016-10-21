95b24e230b6c484be0618cb513769fa66f409b86
tangxiaolong <380562025@qq.com>
2016-10-21 17:22:17 +0800

    格式化代码

--- src/main/java/org/seckill/dao/SeckillDao.java
+++ src/main/java/org/seckill/dao/SeckillDao.java
@@ -7,20 +7,21 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Created by TS on 2016/5/11.
- */
+
 public interface SeckillDao {
 
     //减库存
 
-    int reduceNumber(@Param("seckillId")long seckillId,@Param("killTime")Date killTime);
+    int reduceNumber(@Param("seckillId") long seckillId, @Param("killTime") Date killTime);
+
     //根据Id查询秒杀对象
     Seckill queryById(long seckillId);
+
     //根据偏移量查询秒杀列表
-    List<Seckill> queryAll(@Param("offset")int offset,@Param("limit")int limit);
+    List<Seckill> queryAll(@Param("offset") int offset, @Param("limit") int limit);
+
     /**
      * 使用存储过程执行秒杀
      */
-    void killByProcedure(Map<String,Object> paramMap);
+    void killByProcedure(Map<String, Object> paramMap);
 }
